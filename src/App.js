508bebc91bb1b93fae5c6f1b577cefd7b412b9ce
Naoya-tech <naoya.techwork@gmail.com>
2019-10-08 04:51:28 +0900

    implement action and action creator

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import React, {Component} from 'react';
 const App = () => <Counter />;
 
 class Counter extends Component {
-  constructor(props){
-    super(props);
+  constructor(){
+    super();
     this.state= {
       count: 0
     }
