5e616a9d889fb32537a09d5d038dd4c1a6d2589f
Naoya-tech <naoya.techwork@gmail.com>
2019-10-08 00:47:31 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -10,17 +10,26 @@ import React from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    {name: "Taro", age:10},
+    {name: "Hanako", age:5}
+  ];
+
   return(
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return(
+            <User name={profile.name} age={profile.age} key={index} />
+          );
+        })
+      }
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi!, I am {props.name}, and {props.age} years old.</div>
 }
 
 export default App;
